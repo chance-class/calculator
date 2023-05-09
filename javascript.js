91c164ef8101c3ce45035b57181509b875e2bd7d
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-05-09 18:35:36 -0500

    Add keydown event listener for typing numbers or symbols

--- javascript.js
+++ javascript.js
@@ -43,43 +43,56 @@ console.log(test);
 
 const screen = document.querySelector('#screen');
 
+document.addEventListener('keydown', (event) => {
+    if (!isNaN(Number(event.key))) {
+      screen.textContent += event.key;
+    } else if (event.key === "/" || event.key === "x" || event.key === "+" || event.key === "-" ) {
+      operator = event.key;
+      screen.textContent += event.key;
+      console.log(operator);
+    } else if (event.key === ".") {
+      screen.textContent += event.key;
+    }
+});
+
 const one = document.querySelector('#one');
 one.addEventListener('click', () => {
   screen.textContent += "1";
-})
+});
+
 const two = document.querySelector('#two');
 two.addEventListener('click', () => {
     screen.textContent += "2";
-  })
+  });
 const three = document.querySelector('#three');
 three.addEventListener('click', () => {
     screen.textContent += "3";
-  })
+  });
 const four = document.querySelector('#four');
 four.addEventListener('click', () => {
     screen.textContent += "4";
-  })
+  });
 const five = document.querySelector('#five');
 five.addEventListener('click', () => {
     screen.textContent += "5";
-  })
+  });
 const six = document.querySelector('#six');
 six.addEventListener('click', () => {
     screen.textContent += "6";
-  })
+  });
 const seven = document.querySelector('#seven');
 seven.addEventListener('click', () => {
     screen.textContent += "7";
-  })
+  });
 const eight = document.querySelector('#eight');
 eight.addEventListener('click', () => {
     screen.textContent += "8";
-  })
+  });
 const nine = document.querySelector('#nine');
 nine.addEventListener('click', () => {
     screen.textContent += "9";
-  })
-  const zero = document.querySelector('#zero');
-  zero.addEventListener('click', () => {
-      screen.textContent += "0";
-  })
+  });
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    screen.textContent += "0";
+  });
