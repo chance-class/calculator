bedbfe98a09637882969afe25ba686115f8b4588
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-05-09 17:34:30 -0500

    Add event listeners for numbers

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let n1, operator, n2;
+
 function toAdd (n1, n2) {
   result = (n1 + n2);
   return result;
@@ -38,3 +40,46 @@ function operate (n1, operator, n2) {
 
 let test = operate(5, "+", 3);
 console.log(test);
+
+const screen = document.querySelector('#screen');
+
+const one = document.querySelector('#one');
+one.addEventListener('click', () => {
+  screen.textContent += "1";
+})
+const two = document.querySelector('#two');
+two.addEventListener('click', () => {
+    screen.textContent += "2";
+  })
+const three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    screen.textContent += "3";
+  })
+const four = document.querySelector('#four');
+four.addEventListener('click', () => {
+    screen.textContent += "4";
+  })
+const five = document.querySelector('#five');
+five.addEventListener('click', () => {
+    screen.textContent += "5";
+  })
+const six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    screen.textContent += "6";
+  })
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+    screen.textContent += "7";
+  })
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+    screen.textContent += "8";
+  })
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    screen.textContent += "9";
+  })
+  const zero = document.querySelector('#zero');
+  zero.addEventListener('click', () => {
+      screen.textContent += "0";
+  })
